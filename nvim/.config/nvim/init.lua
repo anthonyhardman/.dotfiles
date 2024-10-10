2d02b860ee83dfc3c61343cc9a183757262d0c7e
= <=>
2024-10-10 13:35:59 -0600

    null ls formatting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -34,6 +34,15 @@ require('mason-lspconfig').setup({
   }
 })
 
+local null_ls = require("null-ls")
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.completion.spell,
+        null_ls.builtins.formatting.black,
+    },
+})
+
 
 local cmp = require('cmp')
 local luasnip = require('luasnip')
