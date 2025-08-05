0db4aee42f2c07b9d6dafa9087b3933b67c046f4
anthonyhardman <anthonythardman@gmail.com>
2025-08-05 06:35:40 -0600

    improve nvim config: enhance telescope, treesitter textobjects, and keymaps
    - Add telescope-ui-select extension for better code action/rename UI
    - Configure treesitter textobjects with function/class/parameter selections and movements
    - Switch telescope find_files to git_files for better project navigation
    - Add descriptive labels to all keymaps for better which-key integration
    - Fix polybar filesystem icon (󰋊 instead of 󰓷)

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 
 -- telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find File" })
+vim.keymap.set('n', '<leader>ff', builtin.git_files, { desc = "Find File" })
 vim.keymap.set('n', '<leader>fF', function() builtin.lsp_document_symbols({ symbols = { 'function', 'method' } }) end,
   { desc = "Find Function" })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live Grep" })
@@ -9,22 +9,22 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find Buffer" })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Help Tags" })
 vim.keymap.set('n', '<leader>fe', builtin.diagnostics, {desc = "Find Errors"})
 
-vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, {})
+vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, { desc = "Show Hover Info" })
 vim.keymap.set('n', 'gd', builtin.lsp_definitions, { desc = "Go to definition" })
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "Go to declaration" })
 vim.keymap.set('n', 'gr', builtin.lsp_references, { desc = "Go to references" })
 vim.keymap.set('n', 'gI', builtin.lsp_implementations, { desc = "Go to implementation" })
 vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, { desc = "Rename Variable" })
 vim.keymap.set('n', '<leader>gq', vim.lsp.buf.format, { desc = "Format Buffer" })
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Line Down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move Line Up" })
 vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, { desc = "Code Action" })
 
-vim.keymap.set("n", "<leader>yy", '"+yy')
-vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>yy", '"+yy', { desc = "Yank Line to Clipboard" })
+vim.keymap.set("v", "<leader>y", '"+y', { desc = "Yank Selection to Clipboard" })
 
-vim.keymap.set("v", "<Tab>", ">gv")
-vim.keymap.set("v", "<S-Tab>", "<gv")
+vim.keymap.set("v", "<Tab>", ">gv", { desc = "Indent Selection" })
+vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Unindent Selection" })
 
-vim.keymap.set('n', '<space>e', function() vim.diagnostic.open_float(0, {scope="line"}) end)
+vim.keymap.set('n', '<space>e', function() vim.diagnostic.open_float(0, {scope="line"}) end, { desc = "Show Line Diagnostics" })
 
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,9 @@
 return {
   'nvim-telescope/telescope.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = { 
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope-ui-select.nvim'
+  },
   config = function()
     require('telescope').setup{
       defaults = {
@@ -11,7 +14,13 @@ return {
           "node_modules/",   -- common exclusion
         },
       },
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown {}
+        }
+      }
     }
+    require("telescope").load_extension("ui-select")
   end,
 }
 
